ab9cbbb86c7f76ca4bc4599c9baabe9b1e2e9b4e
BESHOY\besho <beshoy-beshier@spatiumsftware.com>
2024-02-06 11:08:32 +0200

    init app architecture

---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,20 @@
+import 'package:advanced_topics_in_flutter/core/routing/routes.dart';
+import 'package:advanced_topics_in_flutter/features/no_route/noroute_screen.dart';
+import 'package:flutter/material.dart';
+
+class AppRouter {
+  Route generateRoute(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
+      case Routes.onBoarding:
+        return MaterialPageRoute(builder: (context) => const Placeholder());
+      case Routes.loginScreen:
+        return MaterialPageRoute(builder: (context) => const Placeholder());
+      case Routes.home:
+        return MaterialPageRoute(builder: (context) => const Placeholder());
+
+      default:
+        return MaterialPageRoute(
+            builder: (context) => const NoRouteScreen() );
+    }
+  }
+}
---
+++ lib/core/routing/routes.dart
@@ -0,0 +1,8 @@
+class Routes{
+    static const String onBoarding='/onboarding';
+    static const String loginScreen='/loginScreen';
+    
+
+  static const String home='/home';
+
+}
